fb6cac10684a8303f63a26bbcbf64ea292458d10
Shafi Uddin Khan <skfoysal850@gmail.com>
2025-10-18 20:40:20 +0600

    assignment

---
+++ ClassTasks/task10.java
@@ -0,0 +1,15 @@
+package ClassTasks;
+import java.util.Scanner;
+public class task10 {
+    public static void main(){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+          if(n%2==0){
+            System.out.print("True");
+          }
+          else{
+            System.out.print("False");
+          }
+        sc.close();
+    }
+}
---
+++ ClassTasks/task9.java
@@ -0,0 +1,10 @@
+package ClassTasks;
+import java.util.Scanner;
+public class task9 {
+    public static void main(){
+        Scanner sc=new Scanner(System.in);
+        String n=sc.next();
+        System.out.println(n.charAt(0));
+        sc.close();    
+    }  
+}
