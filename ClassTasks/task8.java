99c571c1d0b1d5d49959820f6ab32b581500d9a2
Shafi Uddin Khan <skfoysal850@gmail.com>
2025-10-18 20:28:37 +0600

    Create task8.java

---
+++ ClassTasks/task8.java
@@ -0,0 +1,12 @@
+package ClassTasks;
+import java.util.Scanner;   
+public class task8 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+       String m=sc.next();
+       String n=sc.next();
+       System.out.print(n+m);
+       sc.close();  
+    }
+}
+
