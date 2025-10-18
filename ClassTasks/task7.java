cedd4b1850d8a268e3b223067875a905a8dc861a
Shafi Uddin Khan <skfoysal850@gmail.com>
2025-10-18 20:26:28 +0600

    Assignment

---
+++ ClassTasks/task7.java
@@ -0,0 +1,14 @@
+package ClassTasks;
+import java.util.Scanner;
+public class task7 {
+        public static void main(){
+      Scanner sc=new Scanner(System.in);
+         System.out.print("Input:");
+         int n=sc.nextInt();
+         int n1=n/4;
+         int maxload=n1*4;
+         System.out.print("max load:"+maxload);
+       sc.close();
+    }
+}
+
