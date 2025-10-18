06e771f81b47815072a088686d7f877bd50dd0ff
Shafi Uddin Khan <skfoysal850@gmail.com>
2025-10-18 20:37:57 +0600

    a

--- ClassTasks/task9.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class task9 {
-    public static void main(){
-        Scanner sc=new Scanner(System.in);
-        String n=sc.next();
-        System.out.println(n.charAt(0));
-        sc.close();    
-    }  
-}
